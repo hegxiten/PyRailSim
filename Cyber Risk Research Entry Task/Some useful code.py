10039edc60eb3f46e2819aef9c51a01ffa2abe4b
KAI787 <33701054+KAI787@users.noreply.github.com>
2018-11-08 09:47:11 -0500

    json in python
    The difference between json and list.
    List <-> Json transfer code.

--- Cyber Risk Research Entry Task/Some useful code.py
+++ Cyber Risk Research Entry Task/Some useful code.py
@@ -10,3 +10,20 @@ with open("/Users/guokai/Desktop/entry_task.txt", 'r') as f2:
         direc.append(line[25:35].replace(' ', ''))
         time_buffer.append(line[59:62].replace(' ', ''))
     print tons, direc, time_buffer
+
+# Main difference between list and json
+list is a set of keys and values, json is a string.
+ex: list = {1: a, 2: b, 3: c}
+    json = '{1: a, 2: b, 3: c}'
+
+# Python list -> Json
+import json
+data = [ { 'a' : 1, 'b' : 2, 'c' : 3, 'd' : 4, 'e' : 5 } ]
+json = json.dumps(data)
+print json
+
+# Json -> Python list
+import json
+jsonData = '{"a":1,"b":2,"c":3,"d":4,"e":5}';
+text = json.loads(jsonData)
+print text
