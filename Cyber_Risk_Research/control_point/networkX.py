265c24ed6d3c7efaded4ecab0cbed52b5bd8d1a2
KAI787 <hdyngk@gmail.com>
2018-11-26 19:08:12 -0500

    constant position of networkX
    output the position of original graph

--- Cyber_Risk_Research/control_point/networkX.py
+++ Cyber_Risk_Research/control_point/networkX.py
@@ -5,30 +5,52 @@ import matplotlib.pyplot as plt
 
 
 def drawnet():
-
-    plt.figure(figsize=(8, 6), dpi=80)
-
     G = nx.Graph()
+    G.add_node('a')
+    G.add_edge('x', 'y')
+    nx.write_gpickle(G, "a.gpickle")
+    G = nx.read_gpickle("a.gpickle")
 
-    tuple = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (4, 8), (5, 8), (3, 7)]
-    G.add_edges_from(tuple)
-    nx.draw(G, node_size=500)
+    # G = nx.florentine_families_graph()
+    # pos = nx.spring_layout(G)
+    # nx.set_node_attributes(G, pos, 'pos')
+    # nx.write_gpickle(G, "florentine_families_graph.gpickle")
 
+    G = nx.read_gpickle("florentine_families_graph.gpickle")
+    pos = nx.get_node_attributes(G, 'pos')
 
-    plt.ion()
-
-    for index in range(10):
-        #plt.cla()
-
-        G[1][3]['color'] = 'blue'
-
-
-        plt.pause(0.1)
-
-    plt.ioff()
-
+    nx.draw(G, pos)
     plt.show()
-    return
+
 drawnet()
 
+    # G = nx.read_gpickle("florentine_families_graph.gpickle")
+    # pos = nx.get_node_attributes(G, 'pos')
+
+
+#     plt.figure(figsize=(8, 6), dpi=80)
+#
+#     G = nx.Graph()
+#
+#     tuple = [(1, 2), (1, 3), (2, 4), (2, 5), (3, 6), (4, 8), (5, 8), (3, 7)]
+#     G.add_edges_from(tuple)
+#     nx.draw(G, node_size=500)
+#
+#
+#     plt.ion()
+#
+#     for index in range(10):
+#         #plt.cla()
+#
+#         G[1][3]['color'] = 'blue'
+#
+#
+#         plt.pause(0.1)
+#
+#     plt.ioff()
+#
+#     plt.show()
+#     return
+# drawnet()
+#
 
