177f1519b440458373a6045f0462b44f678231b5
KAI787 <hdyngk@gmail.com>
2019-01-11 22:37:46 +0100

    late/fast train surpass previous/late trains

--- Cyber_Risk_Research/single_train/main.py
+++ Cyber_Risk_Research/single_train/main.py
@@ -1,15 +1,17 @@
-# '''
+#'''
 # single_train
-from single_train import single_train
+import sum
+from sum import single_train
+sum.networkX_write()
 
-import networkX_w_r
-networkX_w_r.networkX_write()
-
-a = single_train('2018-01-01 00:00:00', '2018-01-02 00:00:00')
+a = single_train('2018-01-01 00:00:00', '2018-01-03 00:00:00', [200, 400, 600, 800])
 print a.generate_all()
-# '''
+#'''
+
 
 '''
 # multi_dirc
-from multi_dirc import multi_dirc
+from sum import multi_dirc
+a = multi_dirc('2018-01-01 00:00:00', '2018-01-02 00:00:00', 200, [50, 100, 150])
+print a.generate_all()
 '''
