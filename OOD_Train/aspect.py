5fcecbb3c81d95a1c999702d4a363ed0c52b740d
Zezhou Wang <wangzezai@hotmail.com>
2019-05-21 19:08:06 -0400

    working on the observation

--- OOD_Train/aspect.py
+++ OOD_Train/aspect.py
@@ -4,10 +4,16 @@ class Aspect(object):
     '''    
     def __init__(self, color):
         self.color = color
+    
+    def __eq__(self,other):
+        return self.color == other.color
+
+    def __ne__(self,other):
+        return self.color != other.color
 
     def __lt__(self,other):
         '''
-        r < y < yy <g
+        r < y < yy < g
         '''  
         if self.color == 'r' and other.color != 'r':
             return True
@@ -17,3 +23,57 @@ class Aspect(object):
             return True
         else:
             return False
+
+    def __gt__(self,other):
+        '''
+        g > yy > y > r
+        '''  
+        if self.color == 'g' and other.color != 'g':
+            return True
+        elif self.color == 'yy' and (other.color == 'y' or other.color == 'r'):
+            return True
+        elif self.color == 'y' and (other.color == 'r'):
+            return True
+        else:
+            return False
+
+    def __le__(self,other):
+        '''
+        r <= y <= yy <= g
+        '''  
+        if self.color == 'r':
+            return True
+        elif self.color == 'y' and (other.color != 'r'):
+            return True
+        elif self.color == 'yy' and (other.color == 'g' or other.color == 'yy'):
+            return True
+        elif self.color == 'g' and other.color == 'g':
+            return True
+        else:
+            return False
+
+    def __ge__(self,other):
+        '''
+        g >= yy >= y >= r
+        '''  
+        if self.color == 'g':
+            return True
+        elif self.color == 'yy' and (other.color != 'g'):
+            return True
+        elif self.color == 'y' and (other.color == 'r' or other.color == 'y'):
+            return True
+        elif self.color == 'r' and other.color == 'r':
+            return True
+        else:
+            return False
+
+if __name__ == '__main__':
+    r1 = Aspect('r')
+    r2 = Aspect('r')
+    y1 = Aspect('y')
+    y2 = Aspect('y')
+    yy1 = Aspect('yy')
+    g1 = Aspect('g')
+    g2 = Aspect('g')
+
+    print(y2 < yy1)
--- OOD_Train/signal_light.py
+++ OOD_Train/signal_light.py
@@ -1,3 +1,5 @@
+from aspect import Aspect
+
 class Observable(object):
     '''
     被观察的对象，实现类需要具体增加被监听的资源
@@ -21,8 +23,6 @@ class Observable(object):
     def listener_updates(self, obj=None):
         for observer in self.__observers:
             observer.update(self, obj)
-            
-from aspect import Aspect
 
 class Observer(object):
     '''
@@ -40,59 +40,65 @@ class Signal(Observable, Observer):
         self.tgt_sp = 0
         self.type = type
         self.facing_direction = facing_direction
+
     def change_color_to(self, color):
+        new_aspect = Aspect(color)
         self.aspect.color = color
         if color == 'g':
-            self.color = 'g'
             self.tgt_sp = self.allow_sp
         elif color == 'yy':
-            self.color = 'yy'
             self.tgt_sp = self.allow_sp * 3 / 4
         elif color == 'y':
-            self.color = 'y'
             self.tgt_sp = self.allow_sp / 2
         else:
-            self.color = 'r'
             self.tgt_sp = 0
-        new = self
-        self.listener_updates(obj=new)
-    def update(self, observing_old, observing_new):
-        if observing_new.aspect < observing_old.aspect:        # 同向下一个信号降灯
-            if observing_new.aspect.color == 'yy':             # g -> yy
-                self.aspect.color = 'g'
-                self.tgt_sp = self.allow_sp 
-            elif observing_new.aspect.color == 'y':            # yy -> y
-                self.aspect.color = 'yy'
-                self.tgt_sp = self.allow_sp * 3 / 4
-            elif observing_new.aspect.color == 'r':            # y -> r
-                self.aspect.color = 'y'
-                self.tgt_sp = self.allow_sp / 2
-        if observing_new > observing_old.aspect:        # 同向下一个信号升灯
-            if observing_new.aspect.color == 'y':       # r -> y
-                self.aspect.color = 'yy'
-                self.tgt_sp = self.allow_sp * 3 / 4
-            elif observing_new.aspect.color == 'yy':    # y -> yy
-                self.aspect.color = 'g'
-                self.tgt_sp = self.allow_sp
-        if observing_new.aspect.color != 'r':           # 反向同位置信号非红
-            self.aspect.color = 'r'
-            self.tgt_sp = 0
+        self.listener_updates(obj=new_aspect)
+
+    def update(self, observing, new_aspect):
+        # print('^^^')
+        # print(self.__dict__['index'], self.__dict__['facing_direction'], self.__dict__['aspect'].color)
+        # print(observing_old.__dict__['index'], observing_old.__dict__['facing_direction'], observing_old.__dict__['aspect'].color)
+        # print(observing_new.__dict__['index'], observing_new.__dict__['facing_direction'], observing_new.__dict__['aspect'].color)
+        if observing.facing_direction == self.facing_direction:
+            if observing.aspect < observing_old.aspect:         # observable drops down
+                print('####')
+                if observing_new.aspect.color == 'yy':              # observable:      g -> yy
+                    self.change_color_to('g')                       # observer:          -> g
+                elif observing_new.aspect.color == 'y':             # observable:   g/yy -> y
+                    self.change_color_to('yy')                      # observer:          -> yy
+                elif observing_new.aspect.color == 'r':             # observable: g/yy/y -> r
+                    self.change_color_to('y')                       # observer:          -> y
+            if observing_new.aspect > observing_old.aspect:         # observable clears up
+                if observing_new.aspect.color == 'y':               # observable:      r -> y
+                    self.change_color_to('yy')                      # observer:          -> yy
+                elif observing_new.aspect.color == 'yy':            # observable:    r/y -> yy
+                    self.change_color_to('g')                       # observer:          -> g
+                elif observing_new.aspect.color == 'g':             # observable:  r/y/yy -> g
+                    self.change_color_to('g')                       # observer:          -> g
+        if observing_new.facing_direction != observing_old.facing_direction:
+            if observing_new.aspect.color != 'r':                   # 任意反向信号非红                 
+                self.change_color_to('r')
 
 if __name__ == '__main__':
     left_signal = [Signal(i, 'abs', 'left', 70) for i in range(8)]
     right_signal = [Signal(i, 'abs', 'right', 70) for i in range(8)]
-    for i in range(8):
-        left_signal[i].add_observer(right_signal[i])
-        right_signal[i].add_observer(left_signal[i])
-    
+
+    for i in range(7):
+        left_signal[i].add_observer(left_signal[i+1])
     for i in range(1,8):
-        left_signal[i].add_observer(left_signal[i-1])
         right_signal[i].add_observer(right_signal[i-1])
+    # add the signal ahead as observer (self being observed by the signal facing ahead)
+
+    for i in range(8):
+        for j in range(8):
+            left_signal[i].add_observer(right_signal[j])
+            right_signal[i].add_observer(left_signal[j])
+
 
     print('left :',[s.aspect.color for s in left_signal])
     print('right:',[s.aspect.color for s in right_signal])
 
-    left_signal[3].change_color_to('yy')
+    left_signal[3].change_color_to('y')
 
     print('left :',[s.aspect.color for s in left_signal])
     print('right:',[s.aspect.color for s in right_signal])
